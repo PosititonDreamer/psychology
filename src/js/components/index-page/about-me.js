4443802048652917b62c9f711bf9e930d8cd94f9
posititonDreamer <nulva12344@gmail.com>
2023-09-20 22:10:25 +0500

    fix about-me

--- src/js/components/index-page/about-me.js
+++ src/js/components/index-page/about-me.js
@@ -1,16 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
   const videos = document.querySelectorAll('.about-me__video')
 
-  if(videos) {
+  if (videos) {
     videos.forEach(videoElement => {
       const play = videoElement.querySelector('.about-me__play-button')
       const video = videoElement.querySelector('video')
-      videoElement.addEventListener('click', () => {
+
+      const played = () => {
         play.classList.add('about-me__play-button--hide')
         video.controls = true
         video.load()
         video.play()
-      })
+        videoElement.removeEventListener('click', played)
+      }
+      videoElement.addEventListener('click', played)
     })
   }
 })
